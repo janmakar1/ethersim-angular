4b860808bbdfa19d936c086a639612c524395385
Janek <jan.kiel665@gmail.com>
2020-09-12 17:43:35 +0200

    Dotenv-cli instead of custom set-env ts script
    Set-env.ts added secrets to envvars, but process.env is not "permanent"

--- scripts/set-env.ts
+++
@@ -1,9 +0,0 @@
-// this is a script running server-side ts-node - adding variables in .env file
-//to system's environment variables. Next
-
-console.log("Set-env");
-
-require('dotenv').config();
-
-//delet this
-console.log(process.env);
